4a29d4534cd38f08ab34ea667106070655510492
Nicolas Dickreuter <dickreuter@gmail.com>
2025-01-26 18:22:50 +0000

    add posthog

--- src/routes/Entry.tsx
+++ src/routes/Entry.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../App.tsx'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
       <App />
-    </React.StrictMode>,
-  )
+    </PostHogProvider>
+  </React.StrictMode>
+)
   
